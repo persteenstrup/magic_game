8c728fb976fce217ec3d563e843365e25c373224
Per Steenstrup <per.steenstrup@gmail.com>
2018-02-07 11:29:54 -0800

    change creature namespace

--- Creature.cs
+++ Creature.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace magic {
+namespace magic_game {
     public abstract class Creature :Card {
         public int attack;
         public int defense;
